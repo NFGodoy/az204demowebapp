86736e84d68730aed0e56048e8a9b6b559486fc5
Newton Godoy <nfdegodoy@yahoo.com.au>
2023-09-08 14:22:12 +1000

    First update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! We deployed from Github to Azure!");
 
 app.Run();
