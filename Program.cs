4129028b057589d8c2ed055762dda8f8c3b18014
Newton Godoy <nfdegodoy@yahoo.com.au>
2023-09-08 18:01:06 +1000

    A message for the commit is requird.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! We deployed from Github to Azure! One further update.");
+app.MapGet("/", () => "Hello World! We deployed from Github to Azure! One further update. Another one.");
 
 app.Run();
